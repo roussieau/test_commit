5212c74b76248475a83c971fb4c96ed606ab95d5
Julian Roussieau <jur+github@odoo.com>
2022-10-17 16:34:52 +0200

    new module

---
+++ mymodule/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Invoicing',
+    'version' : '1.0',
+    'summary': 'mymodule',
+    'sequence': 10,
+    'depends' : ['contacts'],
+    'installable': True,
+    'license': 'LGPL-3',
+}
