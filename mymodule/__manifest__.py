c86b9110dd57049e09c175d834505a0ecb61eda7
Julian Roussieau <jur+github@odoo.com>
2022-10-17 16:44:36 +0200

    add views

--- mymodule/__manifest__.py
+++ mymodule/__manifest__.py
@@ -1,9 +1,12 @@
 {
-    'name' : 'Invoicing',
-    'version' : '1.0',
+    'name': 'Invoicing',
+    'version': '1.0',
     'summary': 'mymodule',
     'sequence': 10,
-    'depends' : ['contacts'],
+    'depends': ['contacts'],
     'installable': True,
     'license': 'LGPL-3',
+    'data': [
+        'views/res_partner_views.xml',
+    ],
 }
